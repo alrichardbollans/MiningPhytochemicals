44c99bb8fdf6d21c099cc98741814a18651f50b4
alrichardbollans <38588335+alrichardbollans@users.noreply.github.com>
2025-11-21 15:02:27 +0000

    Update usage of manual verification

--- extraction/evaluation/evaluating_model_vs_app_outputs.py
+++ extraction/evaluation/evaluating_model_vs_app_outputs.py
@@ -7,22 +7,11 @@ from data.get_wikidata import WCVP_VERSION
 from extraction.methods.extending_model_outputs import resolve_name_to_inchi
 
 
-def get_what_would_be_output_data():
-    found_pairs = pd.read_csv(os.path.join('..', 'app_to_manually_check_methods', 'outputs', 'deepseek', 'found_pairs.csv'))
-    found_pairs['InChIKey'] = found_pairs['compound'].apply(resolve_name_to_inchi)
-    acc_data = get_accepted_info_from_names_in_column(found_pairs,'name', wcvp_version=WCVP_VERSION)
-    acc_data.to_csv(os.path.join('outputs','manually checked output data', 'dataset.csv'), index=False)
-    acc_data.describe(include='all').to_csv(os.path.join('outputs','manually checked output data', 'dataset_summary.csv'))
-
-    resolved_dataset= acc_data.dropna(subset=['accepted_name', 'InChIKey'])
-    resolved_dataset.to_csv(os.path.join('outputs','manually checked output data', 'dataset_resolved.csv'), index=False)
-    resolved_dataset.describe(include='all').to_csv(os.path.join('outputs','manually checked output data', 'dataset_resolved_summary.csv'))
-
-
-def get_precision_scores(model):
-    found_pairs = pd.read_csv(os.path.join('..', 'app_to_manually_check_methods','outputs', model, 'found_pairs.csv'))
-    not_found_pairs = pd.read_csv(os.path.join('..', 'app_to_manually_check_methods','outputs', model, 'not_found_pairs.csv'))
-    assert len(set(found_pairs['doi'].tolist() + not_found_pairs['doi'].tolist())) >= 9
+def get_precision_scores(case):
+    results = pd.read_csv(os.path.join('manual_matching_results', 'manual results', case, 'results.csv'))
+    found_pairs = results[results['decision'] == 'Yes']
+    not_found_pairs = results[results['decision'] == 'No']
+    assert len(set(found_pairs['pkl_file'].tolist() + not_found_pairs['pkl_file'].tolist())) >= 9
 
     true_positives = len(found_pairs)
     false_positives = len(not_found_pairs)
@@ -33,35 +22,17 @@ def get_precision_scores(model):
     return precision
 
 
-def pseudorecall():
-    found_pairs = pd.read_csv(os.path.join('..', 'app_to_manually_check_methods','outputs', 'deepseek', 'found_pairs.csv'))
-    wikidata_found_pairs = pd.read_csv(os.path.join('..', 'app_to_manually_check_methods','outputs', 'wikidata', 'found_pairs.csv'))
-
-    all_pairs = pd.concat([found_pairs, wikidata_found_pairs])
-    all_pairs['name'] = all_pairs['name'].str.lower()
-    all_pairs['name'] = all_pairs['name'].apply(get_species_binomial_from_full_name)
-    all_pairs['compound'] = all_pairs['compound'].str.lower()
-    all_pairs = all_pairs.drop_duplicates(subset=['name', 'compound', 'doi'])
-
-    deepseek_recall = len(found_pairs) / len(all_pairs)
-    wikidata_recall = len(wikidata_found_pairs) / len(all_pairs)
-
-    print(f'deepseek pseudorecall: {deepseek_recall}')
-    return deepseek_recall, wikidata_recall
-
-
 def main():
-    get_what_would_be_output_data()
-    deepseek_recall, wikidata_recall = pseudorecall()
 
-    deepseek_score = get_precision_scores('deepseek')
-    # Note for wikidata, this is marked for verbatim matching which is not a fair comparison
-    wikidata_score = get_precision_scores('wikidata')
-    out_df = pd.DataFrame({'model': ['deepseek', 'wikidata'], 'precision': [deepseek_score, wikidata_score],
-                           'Pseudorecall': [deepseek_recall, wikidata_recall],
-                           'Notes': ['',
-                                     'for wikidata, this is marked for verbatim matching which is not a fair comparison']})
-    out_df.to_csv(os.path.join( 'outputs', 'model_scores_on_validation_data.csv'))
+    deepseek_score = get_precision_scores('validation cases')
+    out_df = pd.DataFrame({'model': ['deepseek'], 'precision': [deepseek_score],
+                           'Notes': ['']})
+    out_df.to_csv(os.path.join('outputs', 'model_scores_on_validation_data.csv'))
+
+    deepseek_score = get_precision_scores('colombian papers')
+    out_df = pd.DataFrame({'model': ['deepseek'], 'precision': [deepseek_score],
+                           'Notes': ['']})
+    out_df.to_csv(os.path.join('outputs', 'model_scores_on_colombian_papers.csv'))
 
 
 if __name__ == '__main__':
