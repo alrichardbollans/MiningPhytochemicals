9a824f27e9df283beb8f0ee0f602965d4f2e6897
alrichardbollans <38588335+alrichardbollans@users.noreply.github.com>
2026-07-27 14:51:43 +0100

    Move method to phytochemMiner

--- analysis/summaries_and_comparisons_of_datasets_and_extractions/remove_classes.py
+++ analysis/summaries_and_comparisons_of_datasets_and_extractions/remove_classes.py
@@ -8,6 +8,7 @@ import json
 import os
 
 import pandas as pd
+from phytochemMiner import get_classes
 
 
 def remove_classes(folder, classes_to_remove):
@@ -16,45 +17,6 @@ def remove_classes(folder, classes_to_remove):
     filtered_occurrences = occurrences[~occurrences['extracted_compound_name'].str.lower().isin(classes_to_remove)]
     filtered_occurrences.to_csv(os.path.join(folder, 'filtered_occurrences.csv'))
     filtered_occurrences.describe(include='all').to_csv(os.path.join(folder, 'filtered_occurrences_summary.csv'))
-
-
-def get_classes():
-    class_json = json.load(open('index_v1.json'))
-    classes = set(class_json['Class'].keys())
-    superclasses = set(class_json['Superclass'].keys())
-    pathways = set(class_json['Pathway'].keys())
-
-    all_classes = classes | superclasses | pathways
-
-    ## Add some classes manually based on those found in cololmbia paper
-    all_classes.add('saponins')
-    all_classes.add('tannins')
-    all_classes.add('flavonoids')
-    all_classes.add('steroids')
-    all_classes.add('non-reducing sugars')
-    all_classes.add('hemolytic saponins')
-    all_classes.add('organic acids')
-    all_classes.add('catechins')
-    all_classes.add('depsides and depsidones')
-    all_classes.add('double olefins')
-    all_classes.add('reducing sugars')
-    all_classes.add('resins')
-    all_classes.add('sesquiterpenolactones')
-    all_classes.add('phenols')
-    all_classes.add('purines')
-    all_classes.add('polyphenols')
-    all_classes.add('triterpenes')
-
-    all_classes_lower = []
-
-    # lower case everything in all classes
-    for class_ in all_classes:
-        all_classes_lower.append(class_.lower())
-
-    assert 'alkaloids' in all_classes_lower
-    assert 'saponins' in all_classes_lower
-
-    return all_classes_lower
 
 
 def main():
