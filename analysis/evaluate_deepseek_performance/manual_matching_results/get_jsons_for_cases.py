4912e2dc900a55a140d207138e09827d54c3cdb9
alrichardbollans <38588335+alrichardbollans@users.noreply.github.com>
2026-08-03 16:24:50 +0100

    Shorten directory names to not break windows path limits

--- analysis/evaluate_deepseek_performance/manual_matching_results/get_jsons_for_cases.py
+++ analysis/evaluate_deepseek_performance/manual_matching_results/get_jsons_for_cases.py
@@ -15,6 +15,7 @@ import shutil
 
 
 def main():
+    print(f'cwd: {os.getcwd()}')
     extracted_jsons_folder = 'extracted_jsons'
     pathlib.Path(extracted_jsons_folder).mkdir(parents=True, exist_ok=True)
     # pathlib.Path(os.path.join(extracted_jsons_folder, 'validation cases')).mkdir(parents=True, exist_ok=True)
@@ -40,13 +41,12 @@ def main():
     # for sanitised_doi in phytochemistry_dois:
     #     json_file = os.path.join(deepseek_jsons_path, sanitised_doi + '.json')
     #     shutil.copyfile(json_file, os.path.join(extracted_jsons_folder, 'phytochemistry papers', sanitised_doi + '.json'))
-
-    pairs_to_check_df = pd.read_csv(os.path.join(
-        '../../summaries_and_comparisons_of_datasets_and_extractions/summaries/deepseek_after_accepted_filter_phytochem_papers_not_in_other_sources/occurrences.csv'))
+    pairs_to_check_df = pd.read_csv(pathlib.Path(
+        '..','..','summaries_and_comparisons_of_datasets_and_extractions','summaries','ds_acptd_filter_pchem_papers_not_in_other_sources','occurrences.csv'))
     pairs_to_check_df['pairs'] = pairs_to_check_df['accepted_name'] + '_' + pairs_to_check_df['InChIKey_simp']
     pairs_to_check = pairs_to_check_df['pairs'].unique().tolist()
     text_dir, phytochemistry_dois = get_sanitised_dois_for_papers('phytochemistry papers')
-    out_folder_name = 'phytochemistry papers not in wikidata or knapsack'
+    out_folder_name = 'pchem hits not in WD or KN'
     pathlib.Path(os.path.join(extracted_jsons_folder, out_folder_name)).mkdir(parents=True, exist_ok=True)
     pathlib.Path(os.path.join('manual results after accepted filter', out_folder_name)).mkdir(parents=True, exist_ok=True)
     for sanitised_doi in phytochemistry_dois:
@@ -67,19 +67,23 @@ def main():
                 pass
             inchi_keys = {}
             inchi_key_simps = {}
+            compounds = []
             for compound in taxon.inchi_keys:
                 inchi_simp = simplify_inchi_key(taxon.inchi_keys[compound])
                 if taxon.accepted_name:
                     if (taxon.accepted_name + '_' + inchi_simp) in pairs_to_check:
                         inchi_keys[compound] = taxon.inchi_keys[compound]
                         inchi_key_simps[compound] = inchi_simp
+                        compounds.append(compound)
             new_taxon.inchi_keys = inchi_keys
             new_taxon.inchi_key_simps = inchi_key_simps
-            new_taxa_list.append(new_taxon)
+            new_taxon.compounds = compounds
+            if len(compounds) > 0:
+                new_taxa_list.append(new_taxon)
 
         new_taxa_data = TaxaData(taxa=new_taxa_list)
         json_out = new_taxa_data.model_dump(mode="json")
-        with open(os.path.join(extracted_jsons_folder, out_folder_name, sanitised_doi + '_not_in_wikidata_knapsack.json'), "w") as file_:
+        with open(pathlib.Path(extracted_jsons_folder, out_folder_name, sanitised_doi + '_not_in_WD_KN.json'), "w") as file_:
             json.dump(json_out, file_)
 
 
